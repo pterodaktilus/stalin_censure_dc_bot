64b1580f91eaeaa874d6b5687b62fd665c8e4f24
pterodaktilus <pterodaktilus@github.com>
2024-10-21 23:53:44 +0200

    fixing some details

--- main.py
+++ main.py
@@ -5,17 +5,29 @@ import csv
 intents = dc.Intents.default()
 intents.message_content = True
 bot = dc.Client(intents=intents)
-token = csv.get_token()
+
+
+
+
+with open('token.csv', 'r') as csvfile:  # this will close the file automatically.
+    reader = csv.reader(csvfile)
+    row = next(reader) 
+    token = str(row[1]).strip() if row[0]=="token" else exit(1)
+
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
 
 @bot.event
 async def on_message(message):
-    print(f'Message from {message.author}: {message.content}')
-    if "mňau" in message.content and message.author != bot.user:
-        await message.delete()
-        await message.channel.send(f"BAD <@{message.author.id}>!!!\n"+f"https://tenor.com/view/andrew-tate-stare-andrew-tate-andrew-tate-sigma-xafer-gif-10165002945664617941")
+    
+    target_messages = ["mňau"]
+    message_to_send = f"BAD <@{message.author.id}>!!!\n"+f"https://tenor.com/view/andrew-tate-stare-andrew-tate-andrew-tate-sigma-xafer-gif-10165002945664617941"
+    
+    for target in target_messages:
+        if target in message.content and message.author != bot.user:
+            await message.delete()
+            await message.channel.send(message_to_send)
 
 bot.run(token)
 
